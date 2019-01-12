107afbd05ab39cf969404bc819c8863555eedaed
Matheus Schneider <matheus.ssm@gmail.com>
2019-01-12 00:29:07 -0200

    update json url

--- app/src/main/java/br/com/schneiderapps/xyzreader/remote/Config.java
+++ app/src/main/java/br/com/schneiderapps/xyzreader/remote/Config.java
@@ -5,15 +5,15 @@ import android.util.Log;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Config {
-    public static String LOG_TAG = Config.class.getSimpleName();
-    public static final URL BASE_URL;
+class Config {
+    private static String LOG_TAG = Config.class.getSimpleName();
+    static final URL BASE_URL;
 
 
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL("https://nspf.github.io/XYZReader/data.json" );
         } catch (MalformedURLException ignored) {
             Log.w(LOG_TAG, ignored.getMessage(), ignored);
         }
